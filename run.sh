6129260b2f96cd0eb1b4dddf5aef05b51e92a7d7
dorimiamn <36445436+dorimiamn@users.noreply.github.com>
2023-12-02 12:33:38 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # 依存関係をインストール
-#yarn install
+yarn install
 
 # prisma/schema.prisma の内容をデータベースに反映
-#npx prisma db push
+npx prisma db push
 
 # Prisma クライアントを作成
-#npx prisma generate
+npx prisma generate
 
 # app/entry.js のビルド
-#npx webpack
+npx webpack
 
 # 環境変数 PORT を指定してアプリケーションを起動
-#yarn start
+yarn start
